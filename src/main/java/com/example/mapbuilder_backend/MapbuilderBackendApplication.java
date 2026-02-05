572a834d68e7e39f93d01d7051c3a824ef32c6c4
lucas.lipis <lucas.lipis@gmx.de>
2026-02-05 11:29:38 +0100

    init commit

---
+++ src/main/java/com/example/mapbuilder_backend/MapbuilderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mapbuilder_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapbuilderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapbuilderBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mapbuilder_backend/MapbuilderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mapbuilder_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapbuilderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
