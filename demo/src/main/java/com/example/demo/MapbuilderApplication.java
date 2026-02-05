4ae5b6a689ee7fd03b82e074ee0344f510462847
lucas.lipis <lucas.lipis@gmx.de>
2026-02-05 11:04:28 +0100

    spring upload

---
+++ demo/src/main/java/com/example/demo/MapbuilderApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapbuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapbuilderApplication.class, args);
+	}
+
+}
